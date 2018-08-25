cb01d722ffe784dfc21aa76baca38bb30597ace8
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-08-25 19:39:29 -0300

    Drawing line done, the points are ordered by x, still need to find a way to draw in correct order

--- Assets/PointStarter.cs
+++ Assets/PointStarter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq;
+
 
 struct Line
 {
@@ -10,56 +12,37 @@ struct Line
 }
 public class PointStarter : MonoBehaviour {
 
-    Vector3[] positions = { new Vector3(-13, 0, 0.5f), new Vector3(-10,0,-11.5f), new Vector3(-10, 0, 9),
-        new Vector3(-4.5f, 0, -2), new Vector3(-1, 0, 8.5f), new Vector3(0.5f, 0, 6), new Vector3(0.5f, 0, -12),
-        new Vector3(2, 0, 12.5f), new Vector3(3.5f, 0, 11), new Vector3(6.5f, 0, 3.2f), new Vector3(7, 0, -10),
-        new Vector3(9, 0, -5), new Vector3(11.5f, 0, -4)}; 
-
+    Pair<float, float>[] a = { new Pair<float, float>(-13,  0.5f), new Pair<float, float>(-10, -11.5f), new Pair<float, float>(-10 , 9),
+    new Pair<float, float>(-4.5f,  -2), new Pair<float, float>(-1,  8.5f), new Pair<float, float>(0.5f,  6), new Pair<float, float>(0.5f,  -12),
+    new Pair<float, float>(2,  12.5f), new Pair<float, float>(3.5f,  11), new Pair<float, float>(6.5f,  3.2f), new Pair<float, float>(7,  -10),
+    new Pair<float, float>(9,  -5), new Pair<float, float>(11.5f,  -4)};
 
     public GameObject pointPrefab;
     GameObject go;
+    public List<GameObject> points = new List<GameObject>();
+    public List<Transform> hullPoints = new List<Transform>();
 
     void Start () {        
-        foreach(Vector3 pos in positions)
-        {
-      //      Instantiate(pointPrefab, pos, Quaternion.identity, this.transform);// as GameObject;
-        }
 
-        Pair<int, int>[] a = { new Pair<int, int>(0,3), new Pair<int, int>(1,1),
-            new Pair<int, int>(2,2), new Pair<int, int>(4,4), new Pair<int, int>(0,0), new Pair<int, int>(1,2),
-            new Pair<int, int>(3,1), new Pair<int, int>(3,3) };
-
-        /*Pair<int, int>[] _positions = { new Pair<int, int>(-13, 0.5f), new Pair<int, int>(-10,0,-11.5f), new Pair<int, int>(-10, 0, 9),
-        new Pair<int, int>(-4.5f, 0, -2), new Pair<int, int>(-1, 0, 8.5f), new Pair<int, int>(0.5f, 0, 6), new Pair<int, int>(0.5f, 0, -12),
-        new Pair<int, int>(2, 0, 12.5f), new Pair<int, int>(3.5f, 0, 11), new Pair<int, int>(6.5f, 0, 3.2f), new Pair<int, int>(7, 0, -10),
-        new Pair<int, int>(9, 0, -5), new Pair<int, int>(11.5f, 0, -4)};*/
-
-        foreach (Pair<int,int> pointPair in a)
+        foreach (Pair<float, float> pointPair in a)
         {
             Vector3 pos = new Vector3(pointPair.First, 0, pointPair.Second);
-            Instantiate(pointPrefab, pos, Quaternion.identity, this.transform);// as GameObject;
+                go = Instantiate(pointPrefab, pos, Quaternion.identity, this.transform) as GameObject;
+                points.Add(go);
         }
-
-
-
-
-
-        int n = 8;
+        int n = 13;
         printHull(a, n);
 
     }
-    // Stores the result (points of convex hull)
-    Stack<Pair<int, int>> hullStack = new Stack<Pair<int, int>>();
     
-    
+// Stores the result (points of convex hull)
+Stack<Pair<float, float>> hullStack = new Stack<Pair<float, float>>();    
 
     // Returns the side of point p with respect to line
     // joining points p1 and p2.
-    int findSide(Pair<int, int> l1, Pair<int, int> l2, Pair<int, int> l)
-    {
-
-       
-        int val = (l.Second - l1.Second) * (l2.First - l1.First) -
+    int findSide(Pair<float, float> l1, Pair<float, float> l2, Pair<float, float> l)
+    {       
+        float val = (l.Second - l1.Second) * (l2.First - l1.First) -
                   (l2.Second - l1.Second) * (l.First - l1.First);
 
        // Debug.Log("L1: (" + l1.First + ", " + l1.Second + ") ");
@@ -76,7 +59,7 @@ public class PointStarter : MonoBehaviour {
     }
     // Returns the square of distance between
     // p1 and p2.
-    int dist(Pair<int, int> p, Pair<int, int> q)
+    float dist(Pair<float, float> p, Pair<float, float> q)
     {
         return (p.Second - q.Second) * (p.Second - q.Second) +
             (p.First - q.First) * (p.First - q.First);
@@ -85,7 +68,7 @@ public class PointStarter : MonoBehaviour {
     // returns a value proportional to the distance
     // between the point p and the line joining the
     // points p1 and p2
-    int lineDist(Pair<int, int> p1, Pair<int, int> p2, Pair<int, int> p)
+    float lineDist(Pair<float, float> p1, Pair<float, float> p2, Pair<float, float> p)
     {
         return Mathf.Abs((p.Second - p1.Second) * (p2.First - p1.First) -
                 (p2.Second - p1.Second) * (p.First - p1.First));
@@ -93,16 +76,16 @@ public class PointStarter : MonoBehaviour {
 
     // End points of line L are p1 and p2. side can have value
     // 1 or -1 specifying each of the parts made by the line L
-    void quickHull(Pair<int, int>[] a, int n, Pair<int, int> p1, Pair<int, int> p2, int side)
+    void quickHull(Pair<float, float>[] a, int n, Pair<float, float> p1, Pair<float, float> p2, int side)
     {
         int ind = -1;
-        int max_dist = 0;
+        float max_dist = 0;
 
         // finding the point with maximum distance
         // from L and also on the specified side of L.
         for (int i = 0; i < n; i++)
         {
-            int temp = lineDist(p1, p2, a[i]);
+            float temp = lineDist(p1, p2, a[i]);
             if (findSide(p1, p2, a[i]) == side && temp > max_dist)
             {
                 ind = i;
@@ -124,7 +107,7 @@ public class PointStarter : MonoBehaviour {
         quickHull(a, n, a[ind], p1, -findSide(a[ind], p1, p2));
         quickHull(a, n, a[ind], p2, -findSide(a[ind], p2, p1));
     }
-    void printHull(Pair<int, int>[] a, int n)
+    void printHull(Pair<float, float>[] a, int n)
     {
         // a[i].second -> y-coordinate of the ith point
         if (n < 3)
@@ -154,23 +137,60 @@ public class PointStarter : MonoBehaviour {
         // a[max_x]
         quickHull(a, n, a[min_x], a[max_x], -1);
 
-        Debug.Log("The points in Convex Hull are:\n");
 
-        int j = 0;
+       foreach (GameObject point in points)
+       {
+            bool belong = false;
+            foreach (Pair<float, float> hull in hullStack)
+            {
+                if (point.transform.position.x == hull.First && point.transform.position.z == hull.Second)
+                {
+                    belong = true;
+                }
+            }
+            if (belong)
+            {
+                hullPoints.Add(point.transform);
+            }
+       }
+
+        hullPoints = hullPoints.OrderBy(go => go.position.x).ToList();
+
+
+        Debug.Log("The points in Convex Hull are:\n");
+        for (int i = 0; i < hullPoints.Count; i++)
+        {
+            if(i == hullPoints.Count - 1)
+            {
+                drawLaser(hullPoints[i], hullPoints[0]);
+            }
+            else
+            {
+                drawLaser(hullPoints[i], hullPoints[i + 1]);
+            }
+
+            Debug.Log("(" + hullPoints[i].position.x + ", " + hullPoints[i].position.z + ") ");
+        }
+        /*
+        Debug.Log("The points in Convex Hull are:\n");
         while (hullStack.Count != 0)
         {
-
             Debug.Log("(" + hullStack.Peek().First + ", " + hullStack.Peek().Second + ") ");
             hullStack.Pop();
-            //hullList.Remove(hullList[0]);
-            j++;
+        }*/
 
-            //  Debug.Log("(" + hull. + ", ");
+    }
+    void drawLaser(Transform start, Transform end)
+    {
+        LineRenderer laser;
+        laser = start.GetComponent<LineRenderer>();
 
-            // cout << "(" << (*hull.begin()).first << ", "
-            //     << (*hull.begin()).second << ") ";
-            // hull.erase(hull.begin());
-        }
+        Vector3 startPos, endPos;
+        startPos = start.position;
+        endPos = end.position;
+
+        laser.SetPosition(0, startPos);
+        laser.SetPosition(1, endPos);
     }
 
 }
